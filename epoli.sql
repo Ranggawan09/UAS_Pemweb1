9afeb4a03ed538575218b6c803632a2e8310cb09
ariff <121781089+Udin78@users.noreply.github.com>
2023-12-18 08:17:17 +0700

    epoli.sql

--- epoli.sql
+++ epoli.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 16 Des 2023 pada 05.47
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.2.12
+-- Generation Time: Dec 18, 2023 at 02:08 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `dokter`
+-- Table structure for table `dokter`
 --
 
 CREATE TABLE `dokter` (
@@ -39,22 +39,22 @@ CREATE TABLE `dokter` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `dokter`
+-- Dumping data for table `dokter`
 --
 
 INSERT INTO `dokter` (`id_dokter`, `namadkt`, `jk`, `alamat`, `telepon`, `jmpraktek`, `tarif`, `id_poli`) VALUES
-(5, 'hari haru', 'Laki-Laki', 'surakarta', '085600929282', 'Senin-Kamis(09.00-15.00)', 150000, 7),
-(6, 'Yanto Mariadi', 'Laki-Laki', 'sukoharjo', '081326530217', 'Senin-Kamis(09.00-15.00)', 175000, 9),
-(7, 'adi', 'DKT-125100', 'jakarta', '087889997654', '', 240000, 8),
-(8, 'desi', 'DKT-125124', 'palembang', '086778987654', '', 210000, 10),
-(9, 'latief', 'DKT-100555', 'jakarta', '087654345678', '', 230000, 11),
-(10, 'nada asmara', 'DKT-100623', 'palembang', '089998765678', '', 340000, 12),
-(11, 'rahmayani', 'DKT-101846', 'surabaya', '086554345678', '', 240000, 7);
+(5, 'Syarif Hidayatullah', 'Laki-Laki', 'Gubeng, Gubeng, Surabaya', '085600929282', 'Senin-Kamis(08.00-15.00)', 150000, 7),
+(6, 'Rudy Ardiansyah', 'Laki-Laki', 'Gading, Jatirejo, Mojokerto', '081326530217', 'Senin-Kamis(14.00-21.00)', 175000, 9),
+(7, 'Heri Purwanto', 'Laki-Laki', 'Sumberejo, Grogol, Kediri', '087889997654', 'Kamis-Senin(08.00-15.00)', 240000, 8),
+(8, 'Arini Al Hikmah', 'Perempuan', 'Banyu Urip, Sawahan, Surabaya', '086778987654', 'Kamis-Senin(14.00-21.00)', 210000, 10),
+(9, 'Wilson Kurniawan', 'Laki-Laki', 'Cideng, Gambir, Jakarta', '087654345678', 'Sabtu-Rabu(08.00-15.00)', 230000, 11),
+(10, 'Nada Asmara', 'Perempuan', 'Karang, Gandus, Palembang', '089998765678', 'Minggu-Rabu(08.00-15.00)', 340000, 12),
+(11, 'Puput Paramita', 'Perempuan', 'Ganting, Gedangan, Sidoarjo', '086554345678', 'Sabtu-Rabu(14.00-21.00)', 240000, 7);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `obat`
+-- Table structure for table `obat`
 --
 
 CREATE TABLE `obat` (
@@ -67,7 +67,7 @@ CREATE TABLE `obat` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `obat`
+-- Dumping data for table `obat`
 --
 
 INSERT INTO `obat` (`id_obat`, `kode_obat`, `nama_obat`, `jenis_obat`, `stok`, `harga_obat`) VALUES
@@ -80,7 +80,7 @@ INSERT INTO `obat` (`id_obat`, `kode_obat`, `nama_obat`, `jenis_obat`, `stok`, `
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pasien`
+-- Table structure for table `pasien`
 --
 
 CREATE TABLE `pasien` (
@@ -93,24 +93,24 @@ CREATE TABLE `pasien` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `pasien`
+-- Dumping data for table `pasien`
 --
 
 INSERT INTO `pasien` (`id_pasien`, `namapsn`, `alamatpsn`, `genderpsn`, `umurpsn`, `teleponpsn`) VALUES
-(2, 'jono', 'surakarta', 'L', 12, '089765145267'),
-(3, 'hadi', 'jakarta', 'L', 19, '08876538893'),
-(4, 'putri', 'palembang', 'P', 19, '086778996554'),
-(5, 'jodi', 'solo', 'L', 19, '087889887665'),
-(6, 'salsa', 'karangasem', 'P', 17, '089889998765'),
-(7, 'tono', 'sukoharjo', 'L', 25, '089998765677'),
-(8, 'adinda', 'surakarta', 'P', 27, '087654321234'),
-(9, 'heni', 'boyolali', 'P', 24, '089765456789'),
-(10, 'Budi Santoso', 'Jl. Mawar,\r\nKedungpari, Mojowarno, Jombang', 'L', 23, '082121212111');
+(2, 'Steven  wiliam', 'Airlangga, Gubeng, Surabaya', 'L', 12, '089765145267'),
+(3, 'Alamul Huda', 'Sukodono, Sukodono, Sidoarjo', 'L', 19, '08876538893'),
+(4, 'Putri Kharisma', 'Tambak, Gunung Anyar, Surabaya', 'P', 19, '086778996554'),
+(5, 'Ahmad Afandi', 'Ngumpul, Jogoroto, Jombang', 'L', 19, '087889887665'),
+(6, 'Salsa Syafira', 'Bringin Canggu, Badas, Kediri', 'P', 17, '089889998765'),
+(7, 'Priyo Utomo', 'Mlancu, Kandangan, Kediri', 'L', 25, '089998765677'),
+(8, 'Syakira Azzahra', 'Mejoyo, Bangsal, Mojokerto', 'P', 27, '087654321234'),
+(9, 'Indri Adelia', 'Claket, Pacet, Mojokerto', 'P', 24, '089765456789'),
+(10, 'Budi Santoso', 'Kedungpari, Mojowarno, Jombang', 'L', 23, '082121212111');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pembayaran`
+-- Table structure for table `pembayaran`
 --
 
 CREATE TABLE `pembayaran` (
@@ -124,7 +124,7 @@ CREATE TABLE `pembayaran` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `pembayaran`
+-- Dumping data for table `pembayaran`
 --
 
 INSERT INTO `pembayaran` (`id_pembayaran`, `nama_pasien`, `jenis_poli`, `tarif_dokter`, `tagihan_obat`, `total_pembayaran`, `tanggal_pembayaran`) VALUES
@@ -144,7 +144,7 @@ INSERT INTO `pembayaran` (`id_pembayaran`, `nama_pasien`, `jenis_poli`, `tarif_d
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pendaftaran`
+-- Table structure for table `pendaftaran`
 --
 
 CREATE TABLE `pendaftaran` (
@@ -157,16 +157,17 @@ CREATE TABLE `pendaftaran` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `pendaftaran`
+-- Dumping data for table `pendaftaran`
 --
 
 INSERT INTO `pendaftaran` (`id_pendaftaran`, `tanggal_daftar`, `dokter`, `pasien`, `poli`, `tarif`) VALUES
-(19, '2023-12-09 02:50:37', 'yanto pelet', 'salsa', 'Kulit', 175000);
+(19, '2023-12-09 02:50:37', 'yanto pelet', 'salsa', 'Kulit', 175000),
+(30, '2023-12-17 15:36:43', 'Syarif Hidayatullah', 'jodi', 'Jantung', 150000);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `poliklinik`
+-- Table structure for table `poliklinik`
 --
 
 CREATE TABLE `poliklinik` (
@@ -176,7 +177,7 @@ CREATE TABLE `poliklinik` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `poliklinik`
+-- Dumping data for table `poliklinik`
 --
 
 INSERT INTO `poliklinik` (`id_poli`, `poli`, `kodeplk`) VALUES
@@ -190,7 +191,7 @@ INSERT INTO `poliklinik` (`id_poli`, `poli`, `kodeplk`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -201,7 +202,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `status`) VALUES
@@ -213,32 +214,32 @@ INSERT INTO `users` (`id`, `username`, `password`, `status`) VALUES
 --
 
 --
--- Indeks untuk tabel `dokter`
+-- Indexes for table `dokter`
 --
 ALTER TABLE `dokter`
   ADD PRIMARY KEY (`id_dokter`),
   ADD KEY `id_poli` (`id_poli`);
 
 --
--- Indeks untuk tabel `obat`
+-- Indexes for table `obat`
 --
 ALTER TABLE `obat`
   ADD PRIMARY KEY (`id_obat`);
 
 --
--- Indeks untuk tabel `pasien`
+-- Indexes for table `pasien`
 --
 ALTER TABLE `pasien`
   ADD PRIMARY KEY (`id_pasien`);
 
 --
--- Indeks untuk tabel `pembayaran`
+-- Indexes for table `pembayaran`
 --
 ALTER TABLE `pembayaran`
   ADD PRIMARY KEY (`id_pembayaran`);
 
 --
--- Indeks untuk tabel `pendaftaran`
+-- Indexes for table `pendaftaran`
 --
 ALTER TABLE `pendaftaran`
   ADD PRIMARY KEY (`id_pendaftaran`),
@@ -248,59 +249,59 @@ ALTER TABLE `pendaftaran`
   ADD KEY `tarif` (`tarif`);
 
 --
--- Indeks untuk tabel `poliklinik`
+-- Indexes for table `poliklinik`
 --
 ALTER TABLE `poliklinik`
   ADD PRIMARY KEY (`id_poli`);
 
 --
--- Indeks untuk tabel `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `dokter`
+-- AUTO_INCREMENT for table `dokter`
 --
 ALTER TABLE `dokter`
   MODIFY `id_dokter` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT untuk tabel `obat`
+-- AUTO_INCREMENT for table `obat`
 --
 ALTER TABLE `obat`
   MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT untuk tabel `pasien`
+-- AUTO_INCREMENT for table `pasien`
 --
 ALTER TABLE `pasien`
   MODIFY `id_pasien` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT untuk tabel `pembayaran`
+-- AUTO_INCREMENT for table `pembayaran`
 --
 ALTER TABLE `pembayaran`
   MODIFY `id_pembayaran` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
--- AUTO_INCREMENT untuk tabel `pendaftaran`
+-- AUTO_INCREMENT for table `pendaftaran`
 --
 ALTER TABLE `pendaftaran`
-  MODIFY `id_pendaftaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id_pendaftaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
--- AUTO_INCREMENT untuk tabel `poliklinik`
+-- AUTO_INCREMENT for table `poliklinik`
 --
 ALTER TABLE `poliklinik`
   MODIFY `id_poli` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
